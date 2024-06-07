d1ff01305490d750831e7af4c92317125b386080
Naga Jagadeesh Budha <nagajagadeesh_budha@infosys.com>
2024-06-07 09:50:38 +0530

    updated IDENTITY_ENDPOINT variable

--- ContainerInstance/start.sh
+++ ContainerInstance/start.sh
@@ -13,8 +13,8 @@ fi
 APPLICATION_ID="499b84ac-1321-427f-aa17-267ca6975798"
 
 #azure container instance to fetch access token
-IMDS_ENDPOINT="http://169.254.169.254/metadata/identity/oauth2/token"
-response=$(curl -s -X GET -H "Metadata: true" "$IMDS_ENDPOINT?resource=$APPLICATION_ID&api-version=2019-08-01")
+IDENTITY_ENDPOINT="http://169.254.169.254/metadata/identity/oauth2/token"
+response=$(curl -s -X GET -H "Metadata: true" "$IDENTITY_ENDPOINT?resource=$APPLICATION_ID&api-version=2019-08-01")
 echo "token response $response"
 status=$?
 if [ $status -ne 0 ]; then
